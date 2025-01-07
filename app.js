69664f1a44d8f71d2225f09f6694bc9472aaae87
dudrnrduddj <cm000519@naver.com>
2025-01-07 16:27:02 +0900

    [add] mysql2 추가, db 연결 코드

--- app.js
+++ app.js
@@ -43,3 +43,47 @@ app.listen(port, () =>{
     console.log("서버가 정상적으로 실행 되었습니다");
 });
 
+
+
+
+
+
+const mysql = require('mysql2')
+
+const pool = mysql.createPool({
+    connectionLimit : 10, // 최대 10개 만듦
+    host : '0.tcp.jp.ngrok.io',
+    user : 'cooksite',
+    password : 'cooksite',
+    database : 'test1',
+    debug : false,
+    port: '14978'
+    // db정보들은 공유되면 안되기 떄문에 파일을 따로만들어 관리
+    // 소스코드와 중요한 정보를 분리시킬 수 있음.
+})
+
+    pool.getConnection((err, conn)=>{
+
+        const exec = conn.query('select * from users;',
+            // 쿼리문을 실행한 이후 콜백 실행 됨
+
+            (err, rows)=>{
+                conn.release();
+                console.log("실행된 SQL query : " + exec.sql);
+                
+
+                // rows에 내장된 속성들이 있음
+                if(rows.length > 0){
+                    console.log("데이터 하나 이상 있음");
+                    console.log(rows[0]);
+                    
+                    return;
+                    
+                }else{
+                    console.log("데이터 없음");
+                    
+                    return;
+                }
+            }
+        )
+    })
