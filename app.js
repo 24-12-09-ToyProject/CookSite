18e70920f4ef0d4e85aec418eb4d0fcd71ee5fd2
Puer <hongbinjo9981@gmail.com>
2025-01-09 20:07:27 +0900

    [ADD] DB 파일 추가 및 파일 구조 수정

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ const nunjucks = require("nunjucks");
 const path = require('path');
 const fs = require('fs');
 
+// db 세팅
+const pool = require('./config/db.js');
+
+
 //body 파싱 하기위한 미들웨어 설정
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -44,46 +48,29 @@ app.listen(port, () =>{
 });
 
 
+//DB 테스트 
+pool.getConnection((err, conn)=>{
 
+    const exec = conn.query('select * from users;',
+        // 쿼리문을 실행한 이후 콜백 실행 됨
 
+        (err, rows)=>{
+            conn.release();
+            console.log("실행된 SQL query : " + exec.sql);
+            
 
-
-const mysql = require('mysql2')
-
-const pool = mysql.createPool({
-    connectionLimit : 10, // 최대 10개 만듦
-    host : '0.tcp.jp.ngrok.io',
-    user : 'cooksite',
-    password : 'cooksite',
-    database : 'test1',
-    debug : false,
-    port: '14978'
-    // db정보들은 공유되면 안되기 떄문에 파일을 따로만들어 관리
-    // 소스코드와 중요한 정보를 분리시킬 수 있음.
-})
-
-    pool.getConnection((err, conn)=>{
-
-        const exec = conn.query('select * from users;',
-            // 쿼리문을 실행한 이후 콜백 실행 됨
-
-            (err, rows)=>{
-                conn.release();
-                console.log("실행된 SQL query : " + exec.sql);
+            // rows에 내장된 속성들이 있음
+            if(rows.length > 0){
+                console.log("데이터 하나 이상 있음");
+                console.log(rows[0]);
                 
-
-                // rows에 내장된 속성들이 있음
-                if(rows.length > 0){
-                    console.log("데이터 하나 이상 있음");
-                    console.log(rows[0]);
-                    
-                    return;
-                    
-                }else{
-                    console.log("데이터 없음");
-                    
-                    return;
-                }
+                return;
+                
+            }else{
+                console.log("데이터 없음");
+                
+                return;
             }
-        )
-    })
+        }
+    )
+})
