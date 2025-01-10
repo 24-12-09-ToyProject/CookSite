bac8810a9a9ecc2c78235b7b7ae114c096f10db6
Puer <hongbinjo9981@gmail.com>
2025-01-10 13:45:23 +0900

    [ADD] 파일 구조 추가

--- config/db.js
+++ config/db.js
@@ -2,12 +2,12 @@ const mysql = require('mysql2')
 
 const pool = mysql.createPool({
     connectionLimit : 10, // 최대 10개 만듦
-    host : '0.tcp.jp.ngrok.io', // host 주소는 고정
+    host : 'localhost', // host 주소는 고정
     user : 'cooksite',
     password : 'cooksite',
     database : 'test1',
     debug : false,
-    port: '13632' // 포트번호 반드시 업데이트 후 사용!! 
+    port: '8800' // 포트번호 반드시 업데이트 후 사용!! 
 
 
     // db정보들은 공유되면 안되기 떄문에 파일을 따로만들어 관리
