9e680c404cd91db9441f710834565f3e9120502b
Puer <hongbinjo9981@gmail.com>
2025-01-23 23:05:51 +0900

    [FIX] db 정보 수정

--- config/db.js
+++ config/db.js
@@ -10,6 +10,6 @@ const pool = mysql.createPool({
     port: 8800 // 포트번호 반드시 업데이트 후 사용!! 
     // db정보들은 공유되면 안되기 떄문에 파일을 따로만들어 관리
     // 소스코드와 중요한 정보를 분리시킬 수 있음.
-}).promise();
+})
 
 module.exports = pool;
