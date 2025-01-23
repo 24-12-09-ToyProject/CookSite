651e8d9d0ad2b86e77f6863c724afcb15eeece67
dudrnrduddj <cm000519@naver.com>
2025-01-23 23:14:19 +0900

    no message

--- config/db.js
+++ config/db.js
@@ -3,13 +3,16 @@ const mysql = require('mysql2')
 const pool = mysql.createPool({
     connectionLimit : 10, // 최대 10개 만듦
     host : 'localhost', // host 주소는 고정
-    user : 'cooksite',
-    password : 'cooksite',
-    database : 'test1',
+    // user : 'cooksite',
+    // password : 'cooksite',
+    // database : 'test1',
+    user : 'root',
+    password : 'khadmin1023!!',
+    database:"cooksite_temp",
     debug : false,
-    port: '8800' // 포트번호 반드시 업데이트 후 사용!! 
-
-
+    port: '3306' // 포트번호 반드시 업데이트 후 사용!! 
+    
+    
     // db정보들은 공유되면 안되기 떄문에 파일을 따로만들어 관리
     // 소스코드와 중요한 정보를 분리시킬 수 있음.
 })
@@ -28,7 +31,7 @@ const pool = mysql.createPool({
 //             console.log("실행된 SQL query : " + exec.sql);
             
 
-//             // rows에 내장된 속성들이 있음
+//             // rows에 내장된 속성들이 있음 
 //             if(rows.length > 0){
 //                 console.log("데이터 하나 이상 있음");
 //                 console.log(rows[0]);
