72de9b57b0f45da59ec1feb7784fb17bdfc606a7
dudrnrduddj <cm000519@naver.com>
2025-02-05 02:34:21 +0900

    [MODIFY] header.js 오류 수정

--- static/js/common/header.js
+++ static/js/common/header.js
@@ -1,18 +1,20 @@
 let myExpandVisible = false;
 
 // 프로필 사진 클릭하면 메뉴 열고 닫기
-// 드롭다운 해보려고 로그인 버튼에 이벤트 달아본거니 프로필 사진 넣고 프사에 이벤트 걸어야됨!!
-document.querySelector("#profile-dropdown-container").addEventListener("click", (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    let expand = document.querySelector(".header-my-expand");
-    if(myExpandVisible) {
-        expand.style.display = 'none';
-    } else {
-        expand.style.display = 'block';
-    }
-    myExpandVisible = !myExpandVisible;
-});
+const profileDropdownContainer = document.querySelector("#profile-dropdown-container");
+if(profileDropdownContainer){
+    profileDropdownContainer.addEventListener("click", (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        let expand = document.querySelector(".header-my-expand");
+        if(myExpandVisible) {
+            expand.style.display = 'none';
+        } else {
+            expand.style.display = 'block';
+        }
+        myExpandVisible = !myExpandVisible;
+    });
+}
 
 // 페이지 내 다른 곳을 클릭하면 메뉴 닫기
 document.addEventListener("click", (e) => {
